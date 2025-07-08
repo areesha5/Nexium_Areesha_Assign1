d3f1a66ae308e5a0f7c81b60d09aa728b3d5b4c0
Aarsh Arsalan <aarsharsalan5@gmail.com>
2025-07-08 08:31:24 +0500

    Add all project files and components

---
+++ src/quotes.ts
@@ -0,0 +1,8 @@
+export const quotes = [
+  { topic: "life", text: "Life is what happens when you're busy making other plans." },
+  { topic: "life", text: "The purpose of our lives is to be happy." },
+  { topic: "life", text: "Get busy living or get busy dying." },
+  { topic: "success", text: "Success is not in what you have, but who you are." },
+  { topic: "success", text: "Don’t be afraid to give up the good to go for the great." },
+  { topic: "success", text: "Success is walking from failure to failure with no loss of enthusiasm." },
+];
